9709be1625cf640f2b2114226e4441afbdbb2ade
Sarah <sarah.celiann@gmail.com>
2023-01-30 10:31:58 -0600

    now getting data from api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import dummyContacts from "../dummyData";
 import { useState } from "react";
 import ContactList from '../ContactList';
 
 const Main = () => {
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
+  const getContacts = async () => {
+    try {
+      const response = await fetch('http://jsonplace-univclone.herokuapp.com/users')
+      const data = await response.json();
+      setContacts(data); 
+    } catch (err) {
+      console.log("We've encountered and error");
+    }
+  }
+  useEffect(() => {  getContacts();}, []);
   contacts.map(contact => {return contact.name});
   return (
     <div id="main">
